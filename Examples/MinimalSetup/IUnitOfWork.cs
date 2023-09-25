c0f02c264a95901fb19d3fc4846cca9012d9ab82
olly armfelt <oliver.armfelt@outlook.com>
2023-09-25 20:40:39 +0100

    Adds Minimal Examples

---
+++ Examples/MinimalSetup/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using CleanCQRS;
+
+namespace MinimalSetup;
+
+public interface IUnitOfWork : IDisposable
+{
+    Task Run(ICommand command);
+    Task<T> Run<T>(ICommand<T> command);
+    Task<T> Run<T>(IQuery<T> query);
+    
+}
---
+++ Examples/MinimalSetup/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using CleanCQRS;
+
+namespace MinimalSetup;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ICQRSRequestHandler requestHandler;
+
+    public UnitOfWork(ICQRSRequestHandler requestHandler)
+    {
+        this.requestHandler = requestHandler;
+    }
+
+    public void Dispose() { }
+
+    public Task Run(ICommand command) => requestHandler.HandleCommand<IUnitOfWork>(this, command, CancellationToken.None);
+    public Task<T> Run<T>(ICommand<T> command) => requestHandler.HandleCommand<IUnitOfWork, T>(this, command, CancellationToken.None);
+    public Task<T> Run<T>(IQuery<T> query) => requestHandler.HandleQuery<IUnitOfWork, T>(this, query, CancellationToken.None);
+}
