f9f595b36cbd4abe1c2f98037a0b782e3d57df07
olly armfelt <oliver.armfelt@outlook.com>
2023-09-25 20:35:49 +0100

    Adds minimal setup tests

---
+++ Examples/MinimalSetupTests/Fakes/TestStore.cs
@@ -0,0 +1,13 @@
+﻿using MinimalSetup;
+
+namespace MinimalSetupTests.Fakes;
+
+// this is a fake dependency (would usually be a fake for interface whos instance is in an outer layer and not in core project)
+public class TestStore : IExampleStore
+{
+    public string LastMessage { get; set; } = "Test Message";
+
+    string IExampleStore.GetLastMessage() => LastMessage;
+
+    void IExampleStore.SetLastMessage(string message) => LastMessage = message;
+}
